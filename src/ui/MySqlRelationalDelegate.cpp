9fbbfb77a077d1229b84a5f040be5216431746a9
xiangxun <806181859@qq.com>
2026-04-09 09:53:28 +0800

    清理合并错误

--- src/ui/MySqlRelationalDelegate.cpp
+++
@@ -1,9 +0,0 @@
-#include "mysqlrelationaldelegate.h"
-
-
-
-void MySqlRelationalDelegate::initStyleOption(QStyleOptionViewItem *option, const QModelIndex &index) const
-{
-    QSqlRelationalDelegate::initStyleOption(option, index);
-    option->displayAlignment = Qt::AlignCenter;
-}
